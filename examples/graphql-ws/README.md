1a33e9812156aeb07f6848abf53b77286779538d
Denis Badurina <badurinadenis@gmail.com>
2021-04-27 17:31:16 +0200

    docs: `graphql-ws` example (#137)
    * genesis
    
    * graphql-ws with ws
    
    * async contextFactory
    
    * fix subscription resolver
    
    * using websockets
    
    * graphql-ws is fully supported
    
    * typo and retitle

---
+++ examples/graphql-ws/README.md
@@ -0,0 +1,9 @@
+## Envelop example with GraphQL-WS
+
+This example demonstrates how to implement the GraphQL flow with Envelop and the [`graphql-ws`](https://github.com/enisdenjo/graphql-ws) WebSocket library.
+
+## Running this example
+
+1. Install all dependencies from the root of the repo (using `yarn`)
+1. `cd` into this folder and run `yarn start`
+1. Copy over the [GraphiQL ❤️ graphql-ws](https://gist.github.com/enisdenjo/a68312878fdc4df299cb0433c60c1dea), change the url to `ws://localhost:3415/graphql` and open the HTML file in the browser
---
+++ examples/graphql-ws/index.ts
@@ -0,0 +1,67 @@
+import { envelop, useSchema, useLogger, useTiming } from '@envelop/core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import ws from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+
+const schema = makeExecutableSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      hello: String!
+    }
+    type Subscription {
+      greetings: String!
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello World!',
+    },
+    Subscription: {
+      greetings: {
+        subscribe: async function* sayHiIn5Languages() {
+          for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
+            yield { greetings: hi };
+            await new Promise(resolve => setTimeout(resolve, 1000)); // wait 1 second
+          }
+        },
+      },
+    },
+  },
+});
+
+const getEnveloped = envelop({
+  plugins: [useSchema(schema), useLogger(), useTiming()],
+});
+
+const { execute, subscribe, parse, validate, contextFactory } = getEnveloped();
+
+useServer(
+  {
+    execute,
+    subscribe,
+    onSubscribe: async (ctx, msg) => {
+      const { schema } = getEnveloped();
+
+      const args = {
+        schema,
+        operationName: msg.payload.operationName,
+        document: parse(msg.payload.query),
+        variableValues: msg.payload.variables,
+        contextValue: await contextFactory({
+          connectionParams: ctx.connectionParams,
+          socket: ctx.extra.socket,
+          request: ctx.extra.request,
+        }),
+      };
+
+      const errors = validate(args.schema, args.document);
+      if (errors.length) return errors;
+
+      return args;
+    },
+  },
+  new ws.Server({
+    port: 3415,
+    path: '/graphql',
+  })
+);
