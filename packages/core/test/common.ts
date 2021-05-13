df30ff64c8fa5302a7449c5069aa8ca756e51204
Laurin Quast <laurinquast@googlemail.com>
2021-05-13 14:12:35 +0200

    refactor: use programmatic schema for subscription support

--- packages/core/test/common.ts
+++ packages/core/test/common.ts
@@ -1,27 +1,69 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import { EventEmitter, on } from 'events';
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 
-export const schema = makeExecutableSchema({
-  typeDefs: /* GraphQL */ `
-    type Query {
-      me: User!
-    }
-    type User {
-      id: ID!
-      name: String!
-    }
-  `,
-  resolvers: {
-    Query: {
-      me: () => {
-        return { _id: 1, firstName: 'Dotan', lastName: 'Simha' };
-      },
+const createPubSub = <TTopicPayload extends { [key: string]: unknown }>(emitter: EventEmitter) => {
+  return {
+    publish: <TTopic extends Extract<keyof TTopicPayload, string>>(topic: TTopic, payload: TTopicPayload[TTopic]) => {
+      emitter.emit(topic as string, payload);
     },
-    User: {
-      id: u => u._id,
-      name: u => `${u.firstName} ${u.lastName}`,
+    subscribe: async function*<TTopic extends Extract<keyof TTopicPayload, string>>(
+      topic: TTopic
+    ): AsyncIterableIterator<TTopicPayload[TTopic]> {
+      const asyncIterator = on(emitter, topic);
+      for await (const [value] of asyncIterator) {
+        yield value;
+      }
+    },
+  };
+};
+
+export const pubSub = createPubSub<{
+  ping: string;
+}>(new EventEmitter());
+
+const GraphQLUser = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+      resolve: u => u._id,
+    },
+    name: {
+      type: GraphQLNonNull(GraphQLString),
+      resolve: u => `${u.firstName} ${u.lastName}`,
     },
   },
 });
+
+const GraphQLQuery = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    me: {
+      type: GraphQLNonNull(GraphQLUser),
+      resolve: () => {
+        return { _id: 1, firstName: 'Dotan', lastName: 'Simha' };
+      },
+    },
+  },
+});
+
+const GraphQLSubscription = new GraphQLObjectType({
+  name: 'Subscription',
+  fields: {
+    ping: {
+      type: GraphQLString,
+      subscribe: async function*() {
+        const stream = pubSub.subscribe('ping');
+        return yield* stream;
+      },
+      resolve: (b, _, context) => {
+        return `${b} ${context}`;
+      },
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({ query: GraphQLQuery, subscription: GraphQLSubscription });
 
 export const query = /* GraphQL */ `
   query me {
@@ -29,5 +71,11 @@ export const query = /* GraphQL */ `
       id
       name
     }
+  }
+`;
+
+export const subscription = /* GraphQL */ `
+  subscription ping {
+    ping
   }
 `;
