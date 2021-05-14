51af526a4950286a9b091eb38c4064cef1758a67
Laurin Quast <laurinquast@googlemail.com>
2021-05-14 08:42:23 +0200

    feat: export utility functions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 export * from '@envelop/types';
 export * from './create';
+export * from './util';
 export * from './plugins/use-logger';
 export * from './plugins/use-timing';
 export * from './plugins/use-schema';
