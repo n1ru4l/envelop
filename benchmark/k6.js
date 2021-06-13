c2fa5c3ce9b516ad69d50282613c6ea4857b6fff
Pablo Sáez <pablosaez1995@gmail.com>
2021-06-13 11:57:15 -0400

    add with-esm example

--- benchmark/k6.js
+++ benchmark/k6.js
@@ -18,7 +18,7 @@ export const options = {
   thresholds: {
     no_errors: ['rate=1.0'],
     expected_result: ['rate=1.0'],
-    http_req_duration: ['p(95)<=12'],
+    http_req_duration: ['p(95)<=15'],
     graphql_execute: ['p(95)<=1'],
     graphql_context: ['p(95)<=1'],
     graphql_validate: ['p(95)<=1'],
