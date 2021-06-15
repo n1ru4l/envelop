5f2ab5e47b08e269d89c9ed0ae29eb35571cc869
Dotan Simha <dotansimha@gmail.com>
2021-06-15 10:32:14 +0300

    fixes

--- benchmark/k6.js
+++ benchmark/k6.js
@@ -18,7 +18,7 @@ export const options = {
   thresholds: {
     no_errors: ['rate=1.0'],
     expected_result: ['rate=1.0'],
-    http_req_duration: ['p(95)<=11'],
+    http_req_duration: ['p(95)<=12'],
     graphql_execute: ['p(95)<=1'],
     graphql_context: ['p(95)<=1'],
     graphql_validate: ['p(95)<=1'],
