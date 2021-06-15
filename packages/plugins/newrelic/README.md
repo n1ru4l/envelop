12c16bd22ab3ddd06d7cc482b2a691ce960f0538
Santino <santinopuleio@gmail.com>
2021-06-15 16:07:23 +0200

    Implement New Relic plugin (#247)
    Co-authored-by: Dotan Simha <dotansimha@gmail.com>

---
+++ packages/plugins/newrelic/README.md
@@ -0,0 +1,101 @@
+## `@envelop/newrelic`
+
+Instrument your GraphQL application with New Relic reporting.  
+Take advantage of Distributed tracing to monitor performance and errors whilst ultimately getting to the root cause of issues.
+
+Below are some screenshots to show how tracking looks like in New Relic, in these examples all plugin options were set to `true`.
+
+Error tracking, operation and resolver views:
+![newrelic_error_operation_screenshot](./error_operation.png)
+![newrelic_error_resolver_screenshot](./error_resolver.png)
+
+Successful operation tracking, operation plus root-field and sub-field resolvers:
+![newrelic_successful_operation_screenshot](./success_operation.png)
+![newrelic_successful_root-field_resolver_screenshot](./success_rootfield_resolver.png)
+![newrelic_successful_sub-field_resolver_screenshot](./success_subfield_resolver.png)
+
+## Getting Started
+
+1. Install dependencies
+2. Configure the New Relic Agent
+3. Setup the New Relic Envelop plugin
+
+## Installation
+
+This plugin expects the Node.js agent, [newrelic npm package](https://www.npmjs.com/package/newrelic), to be installed in your application.
+
+```
+yarn add newrelic @envelop/newrelic
+```
+
+## Basic usage Example
+
+```ts
+import { envelop } from '@envelop/core';
+import { useNewRelic } from '@envelop/newrelic';
+
+const getEnveloped = envelop({
+  plugins: [
+    // ... other plugins ...
+    useNewRelic({
+      includeOperationDocument: true, // default `false`. When set to `true`, includes the GraphQL document defining the operations and fragments
+      includeExecuteVariables: false, // default `false`. When set to `true`, includes all the operation variables with their values
+      includeRawResult: false, // default: `false`. When set to `true`, includes the execution result
+      trackResolvers: true, // default `false`. When set to `true`, track resolvers as segments to monitor their performance
+      includeResolverArgs: false, // default `false`. When set to `true`, includes all the arguments passed to resolvers with their values
+      rootFieldsNaming: true, // default `false`. When set to `true` append the names of operation root fields to the transaction name
+      operationNameProperty: 'hash', // default empty. When passed will check for the property name passed, within the operation object. Will eventually use its value as operation name. Useful for custom operation properties (e.g. queryId/hash)
+    }),
+  ],
+});
+```
+
+> Note: Transaction and segment/span timings may be affected by other plugins used. In order to get more accurate tracking, it is recommended to add the New Relic plugin last.
+
+## Advanced usage
+The plugin allows you to keep control over the variables and arguments that are tracked in New Relic.  
+In addition to the basic `true/false` boolean value, `includeExecuteVariables` and `includeResolverArgs` also accept a RegEx pattern. This allows you to implement white and black listing of properties to be tracked in New Relic.  
+
+This is particularly useful if you have properties coming through variables and arguments that are useful for debugging, but you don't want to leak users' data (such as PII).  
+Below is a quick example of how you can use RegEx to set up white/black listing functionalities.  
+
+```ts
+useNewRelic({
+  includeExecuteVariables: /^(client.*|application.*)$.*$/i, // whitelist, track only variables whose name starts with client or application (e.g. clientName, applicationId)
+  trackResolvers: true, // track resolvers, since we also want to track resolvers' arguments
+  includeResolverArgs: /^(?!(name|email|password)$).*$/, // blacklist, track all arguments whose name does not match 'name', 'email' nor 'password'
+}),
+```
+
+Obviously, the ones above are just a couple of examples, but clearly you have endless options to use any RegEx pattern to filter the variables and arguments in the way that best work for you.  
+
+> Be aware that the only way to filter variables and arguments is to loop through them, so there is an O(_n_) cost when filtering, where _n_ is the number of variables sent to the operation (when tracking execute variables), or the number of arguments passed to resolvers (when tracking resolvers arguments).
+## Agent Configuration
+
+For full details about New Relic Agent configuration, we recommend reading [New Relic's official Node.js agent configuration documentation](https://docs.newrelic.com/docs/agents/nodejs-agent/installation-configuration/nodejs-agent-configuration/).
+
+The main methods to configure the New Relic Agent is through:
+
+- The newrelic.js file [or]
+- The environment variables
+
+If you choose to use the newrelic.js file, then you need this file located in the root of your application. You can look [here](https://github.com/newrelic/node-newrelic/blob/main/newrelic.js) for a basic example of what this file can look like.
+
+If you choose to configure the New Relic Agent through environment variables, then you can follow your preferred strategy to make sure the variables are set and available when your application starts.  
+The variables are the same you can set in newrelic.js file, you just need to know that they need to start with `NEW_RELIC_`, obviously, the variables name must be fully uppercase.
+
+The two variables that are always required are:  
+| Description | newrelic.js | Environment variable |
+| ----------- | ----------- | -------------------- |
+| Application name | `app_name: ['MyAppName']` | `NEW_RELIC_APP_NAME=MyAppName` |
+| License key | `license_key: '40HexadecimalCharacters'` | `NEW_RELIC_LICENSE_KEY=40HexadecimalCharacters` |
+
+Other variables that are popularly used are:  
+| Description | newrelic.js | Environment variable |
+| ----------- | ----------- | -------------------- |
+| Enable distributed tracing | `distributed_tracing: { enabled: true }` | `NEW_RELIC_DISTRIBUTED_TRACING_ENABLED=true` |
+| Logging level | `logging: { level: 'info' }` | `NEW_RELIC_LOG_LEVEL=info` |
+| Capture all request headers | `allow_all_headers: true` | `NEW_RELIC_ALLOW_ALL_HEADERS=true` |
+| Enable error collection | `error_collector: { enabled: true }` | `NEW_RELIC_ERROR_COLLECTOR_ENABLED=true` |
+
+Finally, [here](https://github.com/newrelic/node-newrelic/blob/main/lib/config/default.js) is also a reference of all the configuration variables you can include in your newrelic.js file, or translate into the env variables equivalent.
---
+++ packages/plugins/newrelic/src/index.ts
@@ -0,0 +1,191 @@
+import { shim as instrumentationApi } from 'newrelic';
+import { Plugin, OnExecuteHookResult } from '@envelop/types';
+import { print, FieldNode } from 'graphql';
+import { Path } from 'graphql/jsutils/Path';
+
+enum AttributeName {
+  COMPONENT_NAME = 'Envelop_NewRelic_Plugin',
+  ANONYMOUS_OPERATION = '<anonymous>',
+  EXECUTION_RESULT = 'graphql.execute.result',
+  EXECUTION_OPERATION_NAME = 'graphql.execute.operationName',
+  EXECUTION_OPERATION_TYPE = 'graphql.execute.operationType',
+  EXECUTION_OPERATION_DOCUMENT = 'graphql.execute.document',
+  EXECUTION_VARIABLES = 'graphql.execute.variables',
+  RESOLVER_FIELD_PATH = 'graphql.resolver.fieldPath',
+  RESOLVER_TYPE_NAME = 'graphql.resolver.typeName',
+  RESOLVER_RESULT_TYPE = 'graphql.resolver.resultType',
+  RESOLVER_RESULT = 'graphql.resolver.result',
+  RESOLVER_ARGS = 'graphql.resolver.args',
+}
+
+export type UseNewRelicOptions = {
+  includeOperationDocument?: boolean;
+  includeExecuteVariables?: boolean | RegExp;
+  includeRawResult?: boolean;
+  trackResolvers?: boolean;
+  includeResolverArgs?: boolean | RegExp;
+  rootFieldsNaming?: boolean;
+  operationNameProperty?: string;
+};
+
+interface InternalOptions extends UseNewRelicOptions {
+  isExecuteVariablesRegex?: boolean;
+  isResolverArgsRegex?: boolean;
+}
+
+const DEFAULT_OPTIONS: UseNewRelicOptions = {
+  includeOperationDocument: false,
+  includeExecuteVariables: false,
+  includeRawResult: false,
+  trackResolvers: false,
+  includeResolverArgs: false,
+  rootFieldsNaming: false,
+  operationNameProperty: '',
+};
+
+export const useNewRelic = (rawOptions?: UseNewRelicOptions): Plugin => {
+  const options: InternalOptions = {
+    ...DEFAULT_OPTIONS,
+    ...(rawOptions || {}),
+  };
+  options.isExecuteVariablesRegex = options.includeExecuteVariables instanceof RegExp;
+  options.isResolverArgsRegex = options.includeResolverArgs instanceof RegExp;
+  const logger = instrumentationApi.logger.child({ component: AttributeName.COMPONENT_NAME });
+  logger.info(`${AttributeName.COMPONENT_NAME} registered`);
+
+  instrumentationApi.agent.metrics
+    .getOrCreateMetric(`Supportability/ExternalModules/${AttributeName.COMPONENT_NAME}`)
+    .incrementCallCount();
+
+  return {
+    onExecute({ args }) {
+      const transactionNameState = instrumentationApi.agent.tracer.getTransaction().nameState;
+      const spanContext = instrumentationApi.agent.tracer.getSpanContext();
+      const delimiter = transactionNameState.delimiter;
+      const rootOperation = args.contextValue.operation;
+      const operationType = rootOperation.operation;
+      const document = print(args.contextValue.document);
+      const operationName =
+        rootOperation[options.operationNameProperty as string] || rootOperation.name?.value || AttributeName.ANONYMOUS_OPERATION;
+      const rootFields = rootOperation.selectionSet.selections.map((selection: FieldNode) => selection.name.value);
+
+      transactionNameState.setName(
+        transactionNameState.prefix,
+        transactionNameState.verb,
+        delimiter,
+        operationType + delimiter + operationName + (options.rootFieldsNaming ? delimiter + rootFields.join('&') : '')
+      );
+
+      spanContext.addCustomAttribute(AttributeName.EXECUTION_OPERATION_NAME, operationName);
+      spanContext.addCustomAttribute(AttributeName.EXECUTION_OPERATION_TYPE, operationType);
+      options.includeOperationDocument && spanContext.addCustomAttribute(AttributeName.EXECUTION_OPERATION_DOCUMENT, document);
+
+      if (options.includeExecuteVariables) {
+        const rawVariables = args.variableValues || {};
+        const executeVariablesToTrack = options.isExecuteVariablesRegex
+          ? filterPropertiesByRegex(rawVariables, options.includeExecuteVariables as RegExp)
+          : rawVariables;
+
+        spanContext.addCustomAttribute(AttributeName.EXECUTION_VARIABLES, JSON.stringify(executeVariablesToTrack));
+      }
+
+      const operationSegment = instrumentationApi.getActiveSegment();
+
+      const onResolverCalled: OnExecuteHookResult['onResolverCalled'] = options.trackResolvers
+        ? ({ args: resolversArgs, info }) => {
+            const { returnType, path, parentType } = info;
+            const formattedPath = flattenPath(path, delimiter);
+            const currentSegment = instrumentationApi.getActiveSegment();
+
+            if (!currentSegment) {
+              logger.trace('No active segment found at resolver call. Not recording resolver (%s).', formattedPath);
+              return () => {};
+            }
+
+            const resolverSegment = instrumentationApi.createSegment(
+              `resolver${delimiter}${formattedPath}`,
+              null,
+              operationSegment
+            );
+
+            if (!resolverSegment) {
+              logger.trace('Resolver segment was not created (%s).', formattedPath);
+              return () => {};
+            }
+
+            resolverSegment.start();
+
+            resolverSegment.addAttribute(AttributeName.RESOLVER_FIELD_PATH, formattedPath);
+            resolverSegment.addAttribute(AttributeName.RESOLVER_TYPE_NAME, parentType.toString());
+            resolverSegment.addAttribute(AttributeName.RESOLVER_RESULT_TYPE, returnType.toString());
+
+            if (options.includeResolverArgs) {
+              const rawArgs = resolversArgs || {};
+              const resolverArgsToTrack = options.isResolverArgsRegex
+                ? filterPropertiesByRegex(rawArgs, options.includeResolverArgs as RegExp)
+                : rawArgs;
+
+              resolverSegment.addAttribute(AttributeName.RESOLVER_ARGS, JSON.stringify(resolverArgsToTrack));
+            }
+
+            return ({ result }) => {
+              if (options.includeRawResult) {
+                resolverSegment.addAttribute(AttributeName.RESOLVER_RESULT, JSON.stringify(result));
+              }
+
+              if (result instanceof Error) {
+                const transaction = instrumentationApi.tracer.getTransaction();
+                instrumentationApi.agent.errors.add(transaction, JSON.stringify(result));
+              }
+
+              resolverSegment.end();
+            };
+          }
+        : undefined;
+
+      return {
+        onResolverCalled,
+        onExecuteDone({ result }) {
+          if (result.data && options.includeRawResult) {
+            spanContext.addCustomAttribute(AttributeName.EXECUTION_RESULT, JSON.stringify(result));
+          }
+
+          if (result.errors && result.errors.length > 0) {
+            const agent = instrumentationApi.agent;
+            const transaction = instrumentationApi.tracer.getTransaction();
+
+            for (const error of result.errors) {
+              agent.errors.add(transaction, JSON.stringify(error));
+            }
+          }
+
+          operationSegment.end();
+        },
+      };
+    },
+  };
+};
+
+function flattenPath(fieldPath: Path, delimiter = '/') {
+  const pathArray = [];
+  let thisPath: Path | undefined = fieldPath;
+
+  while (thisPath) {
+    if (typeof thisPath.key !== 'number') {
+      pathArray.push(thisPath.key);
+    }
+    thisPath = thisPath.prev;
+  }
+
+  return pathArray.reverse().join(delimiter);
+}
+
+function filterPropertiesByRegex(initialObject: { [key: string]: any }, pattern: RegExp) {
+  const filteredObject = {};
+
+  for (const property of Object.keys(initialObject)) {
+    if (pattern.test(property)) filteredObject[property] = initialObject[property];
+  }
+
+  return filteredObject;
+}
