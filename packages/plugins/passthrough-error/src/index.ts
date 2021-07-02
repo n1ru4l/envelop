69573149e0a9032ad0cc18325a8d7e280acd6874
Dotan Simha <dotansimha@gmail.com>
2021-07-02 10:12:58 +0300

    wip

--- packages/plugins/passthrough-error/src/index.ts
+++ packages/plugins/passthrough-error/src/index.ts
@@ -7,14 +7,18 @@ export const useErrorPassthrough = (): Plugin<{
   [shortCircuteSymbol]?: Error;
 }> => ({
   onParse({ extendContext }) {
-    return ({ result }) => {
+    return ({ result, replaceParseResult }) => {
       if (result instanceof Error) {
         extendContext({
           [shortCircuteSymbol]: result,
         });
+
+        // Probably this is needed as well
+        // replaceParseResult(null);
       }
     };
   },
+  // onValidate should just passthrough as well
   // Maybe we need to hook into onContextBuilding as well?
   // And stop the flow there? Just to make sure we don't run if it not needed?
   onExecute({ args, setResultAndStopExecution }) {
