21af7d13d48499497d2c9770dcab4f9b0a162e41
Dotan Simha <dotansimha@gmail.com>
2021-07-02 10:10:08 +0300

    example for passthourgh of error

---
+++ packages/plugins/passthrough-error/src/index.ts
@@ -0,0 +1,32 @@
+import { Plugin } from '@envelop/types';
+import { GraphQLError } from 'graphql';
+
+const shortCircuteSymbol = Symbol('shortCircuteSymbol');
+
+export const useErrorPassthrough = (): Plugin<{
+  [shortCircuteSymbol]?: Error;
+}> => ({
+  onParse({ extendContext }) {
+    return ({ result }) => {
+      if (result instanceof Error) {
+        extendContext({
+          [shortCircuteSymbol]: result,
+        });
+      }
+    };
+  },
+  // Maybe we need to hook into onContextBuilding as well?
+  // And stop the flow there? Just to make sure we don't run if it not needed?
+  onExecute({ args, setResultAndStopExecution }) {
+    if (args.contextValue[shortCircuteSymbol]) {
+      setResultAndStopExecution({
+        data: undefined,
+        errors: [
+          args.contextValue[shortCircuteSymbol] instanceof GraphQLError
+            ? args.contextValue[shortCircuteSymbol]
+            : new GraphQLError(args.contextValue[shortCircuteSymbol].message),
+        ],
+      });
+    }
+  },
+});
---
+++ packages/plugins/passthrough-error/test/test.spec.ts
@@ -0,0 +1,49 @@
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { createTestkit } from '@envelop/testing';
+import { usePreloadAssets } from '../src';
+import { useExtendContext } from '@envelop/core';
+
+describe('usePreloadAssets', () => {
+  const imageUrl = 'https://localhost/some-asset.png';
+  const schema = makeExecutableSchema({
+    typeDefs: `type Query { imageUrl: String! noAsset: String! }`,
+    resolvers: {
+      Query: {
+        imageUrl: (_: unknown, __: unknown, context: any) => {
+          context.registerPreloadAsset(imageUrl);
+          return Promise.resolve(imageUrl);
+        },
+        noAsset: () => Promise.resolve('hi'),
+      },
+    },
+  });
+
+  it('Should include assets to preload', async () => {
+    const testInstance = createTestkit([usePreloadAssets()], schema);
+    const result = await testInstance.execute(`query { imageUrl }`);
+    expect(result.errors).toBeUndefined();
+    expect(result.extensions).toEqual({
+      preloadAssets: ['https://localhost/some-asset.png'],
+    });
+  });
+
+  it('Should not include the preload extension if no asset should be preloaded', async () => {
+    const testInstance = createTestkit([usePreloadAssets()], schema);
+    const result = await testInstance.execute(`query { noAsset }`);
+    expect(result.errors).toBeUndefined();
+    expect(result.extensions).toBeUndefined();
+  });
+
+  it('Should not include preload extension if asset preloading is disabled via shouldPreloadAssets', async () => {
+    const testInstance = createTestkit(
+      [
+        useExtendContext(() => ({ shouldPreloadAssets: false })),
+        usePreloadAssets({ shouldPreloadAssets: context => (context as any).shouldPreloadAssets }),
+      ],
+      schema
+    );
+    const result = await testInstance.execute(`query { imageUrl }`);
+    expect(result.errors).toBeUndefined();
+    expect(result.extensions).toBeUndefined();
+  });
+});
