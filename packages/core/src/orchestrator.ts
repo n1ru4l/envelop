51e6643ee6525785c7f5a3fcddf94f63aa2fbc35
Laurin Quast <laurinquast@googlemail.com>
2021-07-16 09:22:26 +0200

    fix import...

--- packages/core/src/orchestrator.ts
+++ packages/core/src/orchestrator.ts
@@ -19,7 +19,7 @@ import {
   TypedExecutionArgs,
   SubscribeFunction,
 } from '@envelop/types';
-import { isAsyncIterable } from 'graphql/jsutils/isAsyncIterable';
+import isAsyncIterable from 'graphql/jsutils/isAsyncIterable';
 import {
   DocumentNode,
   execute,
