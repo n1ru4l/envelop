65137a114217891027f0c0a1152e1ac685ca614c
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-08-18 09:00:05 +0000

    Reproduce an issue in Redwood

--- packages/core/test/context.spec.ts
+++ packages/core/test/context.spec.ts
@@ -1,5 +1,11 @@
 import { createSpiedPlugin, createTestkit } from '@envelop/testing';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ExecutionResult } from 'graphql';
 import { schema, query } from './common';
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 describe('contextFactory', () => {
   it('Should call before parse and after parse correctly', async () => {
@@ -108,5 +114,57 @@ describe('contextFactory', () => {
         }),
       })
     );
+  });
+
+  test('plugins should await for each other when building context', async () => {
+    const afterContextSpy = jest.fn();
+    const testSchema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          authenticated: Boolean!
+        }
+      `,
+      resolvers: {
+        Query: {
+          authenticated(_, __, context) {
+            return context.authenticated;
+          },
+        },
+      },
+    });
+    const teskit = createTestkit(
+      [
+        {
+          async onContextBuilding({ extendContext }) {
+            await sleep(200);
+            extendContext({
+              authenticated: true,
+            });
+          },
+        },
+        {
+          onContextBuilding() {
+            return ({ context }) => {
+              afterContextSpy(context.authenticated);
+            };
+          },
+        },
+      ],
+      testSchema
+    );
+
+    const result = (await teskit.execute(
+      /* GraphQL */ `
+        {
+          authenticated
+        }
+      `,
+      {},
+      {}
+    )) as ExecutionResult;
+
+    expect(result.errors).not.toBeDefined();
+    expect(result.data?.authenticated).toBe(true);
+    expect(afterContextSpy).toHaveBeenCalledWith(true);
   });
 });
