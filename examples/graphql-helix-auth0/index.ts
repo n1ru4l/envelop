224d47240fd6a32a2a73f849587b59a86bec0d8c
David Thyresson <dthyresson@gmail.com>
2021-10-18 03:31:22 -0400

    Tell fastify a response was sent (#834)

--- examples/graphql-helix-auth0/index.ts
+++ examples/graphql-helix-auth0/index.ts
@@ -111,6 +111,9 @@ app.route({
       });
 
       sendResult(result, res.raw);
+
+      // Tell fastify a response was sent
+      res.sent = true;
     }
   },
 });
--- examples/graphql-helix-defer-stream/index.ts
+++ examples/graphql-helix-defer-stream/index.ts
@@ -188,6 +188,9 @@ app.route({
       });
 
       sendResult(result, res.raw);
+
+      // Tell fastify a response was sent
+      res.sent = true;
     }
   },
 });
