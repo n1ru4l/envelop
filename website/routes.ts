97ee6765a40163eda34bde7c29c35ccd96040a44
Laurin Quast <laurinquast@googlemail.com>
2021-12-02 16:15:03 +0100

    fix: make documentation structure flat

--- website/routes.ts
+++ website/routes.ts
@@ -4,33 +4,40 @@ export function getRoutes(): IRoutes {
   const Routes: IRoutes = {
     _: {
       docs: {
-        $name: 'Docs',
-        $routes: ['README', 'getting-started', 'integrations', 'core', 'composing-envelop'],
-        _: {
-          plugins: {
-            $name: 'Plugins',
-            $routes: ['README', 'custom-plugin', 'lifecycle', 'testing'],
-          },
-          guides: {
-            $name: 'Guides',
-            $routes: [
-              'securing-your-graphql-api',
-              'adding-authentication-with-auth0',
-              'monitoring-and-tracing',
-              'using-graphql-features-from-the-future',
-              'resolving-subscription-data-loader-caching-issues',
-              'adding-a-graphql-response-cache',
-            ],
-          },
-        },
+        $name: 'Getting Started',
+        $routes: [
+          ['README', 'Introduction'],
+          ['getting-started', 'Installation'],
+          ['integrations', 'Integrations and Examples'],
+          ['core', '@envelop/core'],
+          ['composing-envelop', 'Sharing Envelops'],
+        ],
+      },
+      'docs/guides': {
+        $name: 'Guides',
+        $routes: [
+          ['securing-your-graphql-api', 'Securing Your GraphQL API'],
+          ['adding-authentication-with-auth0', 'Authentication with Auth0'],
+          ['monitoring-and-tracing', 'Monitoring and Tracing'],
+          ['using-graphql-features-from-the-future', 'GraphQL Features from the Future'],
+          ['resolving-subscription-data-loader-caching-issues', 'Resolving subscription DataLoader Cache Issues'],
+          ['adding-a-graphql-response-cache', 'GraphQL Response Caching'],
+        ],
+      },
+      'docs/plugins': {
+        $name: 'Plugins',
+        $routes: [
+          ['README', 'Introduction'],
+          ['custom-plugin', 'Building Plugins'],
+          ['lifecycle', 'Lifecycle'],
+          ['testing', 'Testing'],
+        ],
       },
     },
   };
 
   GenerateRoutes({
     Routes,
-    folderPattern: 'docs',
-    basePath: 'docs',
   });
 
   return Routes;
