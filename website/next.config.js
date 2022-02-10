8710c95e1bd9d21e236c634ab795430132e86e10
Laurin Quast <laurinquast@googlemail.com>
2022-02-10 16:45:43 +0100

    Update next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -14,4 +14,41 @@ module.exports = withGuildDocs({
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      /* Guides */
+      {
+        source: '/docs/securing-your-graphql-api',
+        destination: '/docs/guides/securing-your-graphql-api',
+        permanent: true,
+      },
+      {
+        source: '/docs/adding-authentication-with-auth0',
+        destination: '/docs/guides/adding-authentication-with-auth0',
+        permanent: true,
+      },
+      {
+        source: '/docs/monitoring-and-tracing',
+        destination: '/docs/guides/monitoring-and-tracing',
+        permanent: true,
+      },
+      {
+        source: '/docs/using-graphql-features-from-the-future',
+        destination: '/docs/guides/using-graphql-features-from-the-future',
+        permanent: true,
+      },
+      {
+        source: '/docs/resolving-subscription-data-loader-caching-issues',
+        destination: '/docs/guides/resolving-subscription-data-loader-caching-issues',
+        permanent: true,
+      },
+      {
+        source: '/docs/adding-a-graphql-response-cache',
+        destination: '/docs/guides/adding-a-graphql-response-cache',
+        permanent: true,
+      },
+    ];
+  }
 });
+
+
