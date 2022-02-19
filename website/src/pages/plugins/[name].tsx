e47d153b77c74480d95bd835ea9a87a54dfac655
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-02-19 08:44:39 -0500

    docs: add meta tags to plugin hub

--- website/src/pages/plugins/[name].tsx
+++ website/src/pages/plugins/[name].tsx
@@ -3,13 +3,14 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import Link from 'next/link';
 import React from 'react';
 import tw, { styled } from 'twin.macro';
-
+import { cleanMarkdown } from '@guild-docs/client/utils';
 import { Box, Center, Code, Container, Grid, SimpleGrid } from '@chakra-ui/react';
 import { PackageInstall, RemoteGHMarkdown } from '@guild-docs/client';
 import { buildMDX, CompiledMDX } from '@guild-docs/server';
 import { getPackagesData, PackageWithStats } from '@guild-docs/server/npm';
 
 import { pluginsArr as packageList } from '../../lib/plugins';
+import Head from 'next/head';
 
 export const SubTitle = styled.h2(() => [tw`mt-0 mb-4 font-bold text-lg md:text-xl`]);
 export const Title = styled.h2(() => [tw`mt-0 mb-4 font-bold text-xl md:text-2xl`]);
@@ -83,58 +84,86 @@ export default function PluginPageContent({ data }: PluginPageProps) {
 
   const pluginData = data[0];
 
+  const description = pluginData.readme
+    ? // remove all markdown
+      cleanMarkdown(pluginData.readme)
+        // we need description and it is after the package name
+        .split(pluginData.npmPackage)
+        .filter(Boolean)[0]
+        // and before getting started section
+        .split('Getting Started')[0]
+        .split('\n')
+        .filter(Boolean)
+        .join(' ')
+    : null;
+
+  const title = `${pluginData.title} | Envelop Plugin Hub`;
+
   return (
-    <section>
-      <Container p={'1.5rem'} maxWidth={1200}>
-        <Title>
-          <Link href="/plugins" passHref>
-            <a>Plugin Hub</a>
-          </Link>
-          {' >'} {pluginData.title}
-        </Title>
-        <Grid templateColumns={['1fr', '1fr', '1fr 350px']} gap={4}>
-          <Box>
-            <PackageInstall packages={pluginData.npmPackage} />
-            <RemoteGHMarkdown
-              directory={pluginData.stats?.repositoryDirectory}
-              repo={pluginData.stats?.repositoryLink}
-              content={pluginData.mdx}
-            />
-          </Box>
-          <Box gridRow={['1', '1', 'auto']}>
-            <SubTitle>Plugin Details</SubTitle>
-            <SimpleGrid columns={2}>
-              <div>Package</div>
-              <div>
-                <StyledLink href={`https://www.npmjs.com/package/${pluginData.npmPackage}`}>
-                  <CodeLink as="span">{pluginData.npmPackage}</CodeLink>
-                </StyledLink>
-              </div>
-              {pluginData.stats?.license ? (
-                <>
-                  <div>License</div>
-                  <div>
-                    <Code>{pluginData.stats.license}</Code>
-                  </div>
-                </>
-              ) : null}
-              {pluginData.stats?.version ? (
-                <>
-                  <div>Version</div>
-                  <div>
-                    <Code>{pluginData.stats.version}</Code>
-                  </div>
-                </>
-              ) : null}
-              {pluginData.stats?.modifiedDate ? (
-                <>
-                  <div>Updated</div>
-                  <div>
-                    <Code>{format(new Date(pluginData.stats.modifiedDate), 'MMM do, yyyy')}</Code>
-                  </div>
-                </>
-              ) : null}
-              {/* {pluginData.stats?.collected?.github?.starsCount ? (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="og:title" content={title} />
+        <meta name="twitter:title" content={title} />
+        {description && (
+          <>
+            <meta name="twitter:description" content={description} />
+            <meta name="og:description" content={description} />
+            <meta name="description" content={description} />
+          </>
+        )}
+      </Head>
+      <section>
+        <Container p={'1.5rem'} maxWidth={1200}>
+          <Title>
+            <Link href="/plugins" passHref>
+              <a>Plugin Hub</a>
+            </Link>
+            {' >'} {pluginData.title}
+          </Title>
+          <Grid templateColumns={['1fr', '1fr', '1fr 350px']} gap={4}>
+            <Box>
+              <PackageInstall packages={pluginData.npmPackage} />
+              <RemoteGHMarkdown
+                directory={pluginData.stats?.repositoryDirectory}
+                repo={pluginData.stats?.repositoryLink}
+                content={pluginData.mdx}
+              />
+            </Box>
+            <Box gridRow={['1', '1', 'auto']}>
+              <SubTitle>Plugin Details</SubTitle>
+              <SimpleGrid columns={2}>
+                <div>Package</div>
+                <div>
+                  <StyledLink href={`https://www.npmjs.com/package/${pluginData.npmPackage}`}>
+                    <CodeLink as="span">{pluginData.npmPackage}</CodeLink>
+                  </StyledLink>
+                </div>
+                {pluginData.stats?.license ? (
+                  <>
+                    <div>License</div>
+                    <div>
+                      <Code>{pluginData.stats.license}</Code>
+                    </div>
+                  </>
+                ) : null}
+                {pluginData.stats?.version ? (
+                  <>
+                    <div>Version</div>
+                    <div>
+                      <Code>{pluginData.stats.version}</Code>
+                    </div>
+                  </>
+                ) : null}
+                {pluginData.stats?.modifiedDate ? (
+                  <>
+                    <div>Updated</div>
+                    <div>
+                      <Code>{format(new Date(pluginData.stats.modifiedDate), 'MMM do, yyyy')}</Code>
+                    </div>
+                  </>
+                ) : null}
+                {/* {pluginData.stats?.collected?.github?.starsCount ? (
                 <>
                   <div>Stars</div>
                   <div>
@@ -142,10 +171,11 @@ export default function PluginPageContent({ data }: PluginPageProps) {
                   </div>
                 </>
               ) : null} */}
-            </SimpleGrid>
-          </Box>
-        </Grid>
-      </Container>
-    </section>
+              </SimpleGrid>
+            </Box>
+          </Grid>
+        </Container>
+      </section>
+    </>
   );
 }
