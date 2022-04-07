dd21c1e0c466dd576a0c6be7ce4c26f33a785db1
Laurin Quast <laurinquast@googlemail.com>
2022-04-07 11:11:25 +0200

    test: add failing sentry setData test

---
+++ packages/plugins/sentry/test/use-sentry.spec.ts
@@ -0,0 +1,25 @@
+import { assertSingleExecutionValue, createTestkit } from '@envelop/testing';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { useSentry } from '../src';
+
+describe('useSentry', () => {
+  it('works', async () => {
+    const schema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          foo: String
+        }
+      `,
+      resolvers: {
+        Query: {
+          foo: () => '1',
+        },
+      },
+    });
+    const testkit = createTestkit([useSentry()], schema);
+
+    const result = await testkit.execute(`{ foo }`);
+    assertSingleExecutionValue(result);
+    expect(result.errors).toBeUndefined();
+  });
+});
