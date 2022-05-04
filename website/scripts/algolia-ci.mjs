57105e7d6dcfde767c16c69536844b9b3b687b31
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-05-04 12:49:09 +0200

    fix algolia script

---
+++ website/scripts/algolia-ci.mjs
@@ -0,0 +1,19 @@
+import { createRequire } from 'node:module';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { indexToAlgolia } from '@guild-docs/algolia';
+import { register } from 'esbuild-register/dist/node.js';
+
+register({ extensions: ['.ts', '.tsx'] });
+
+const require = createRequire(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const { getRoutes } = require('../routes.ts');
+
+indexToAlgolia({
+  routes: [getRoutes()],
+  source: 'Envelop',
+  dryMode: process.env.ALGOLIA_DRY_RUN === 'true',
+  lockfilePath: resolve(__dirname, '../algolia-lockfile.txt'),
+});
