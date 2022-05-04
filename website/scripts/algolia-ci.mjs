77f1873e1eb8cd24d58c4af85878a40736c8d8c2
Charly POLY <cpoly55@gmail.com>
2022-05-04 17:31:15 +0200

    feat(website/search): add plugins

--- website/scripts/algolia-ci.mjs
+++ website/scripts/algolia-ci.mjs
@@ -3,6 +3,7 @@ import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { indexToAlgolia } from '@guild-docs/algolia';
 import { register } from 'esbuild-register/dist/node.js';
+import { getPackagesData } from '@guild-docs/server/npm';
 
 register({ extensions: ['.ts', '.tsx'] });
 
@@ -10,11 +11,15 @@ const require = createRequire(import.meta.url);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const { getRoutes } = require('../routes.ts');
+const { pluginsArr } = require('../src/lib/plugins.ts');
 
-indexToAlgolia({
-  routes: [getRoutes()],
-  source: 'Envelop',
-  dryMode: process.env.ALGOLIA_DRY_RUN === 'true',
-  domain: process.env.SITE_URL,
-  lockfilePath: resolve(__dirname, '../algolia-lockfile.json'),
+getPackagesData({ packageList: pluginsArr }).then(plugins => {
+  indexToAlgolia({
+    routes: [getRoutes()],
+    plugins: plugins,
+    source: 'Envelop',
+    dryMode: process.env.ALGOLIA_DRY_RUN === 'true',
+    domain: process.env.SITE_URL,
+    lockfilePath: resolve(__dirname, '../algolia-lockfile.json'),
+  });
 });
