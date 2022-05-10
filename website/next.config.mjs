1637bfc5684d8f17e11da8fa28b37dd836f0dba9
Charly POLY <1252066+charlypoly@users.noreply.github.com>
2022-05-10 09:43:59 +0200

    feat(search): new UI + algolia-sync script + CI workflows (#1383)
    * WIP new algolia search
    
    * fix algolia script
    
    * feat(website): algolia CI
    
    * feat(website): enhance search results
    
    * fix(website): search fix
    
    * refactor(website): algolia-lockfile.txt -> algolia-lockfile.json
    
    * feat(website): update search UI to v2
    
    * feat(website/search): add plugins
    
    * feat(website): update `@theguild/components`
    
    * chore(website): update `@theguild/components`
    
    * fix(ci): algolia workflows
    
    * chore(website): `@theguild/components` and `@guild-docs/algolia` stable versions
    
    * fix(ci): algolia workflows -> prettier
    
    * fix(website): update `@theguild/components`
    
    * fix(website): nav + search fixes by updating `@theguild/components`
    
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,4 @@
-import { createRequire } from 'module';
+import { createRequire } from 'node:module';
 import { withGuildDocs } from '@guild-docs/server';
 import { register } from 'esbuild-register/dist/node.js';
 import { i18n } from './next-i18next.config.js';
@@ -14,39 +14,37 @@ export default withGuildDocs({
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async redirects() {
-    return [
-      /* Guides */
-      {
-        source: '/docs/securing-your-graphql-api',
-        destination: '/docs/guides/securing-your-graphql-api',
-        permanent: true,
-      },
-      {
-        source: '/docs/adding-authentication-with-auth0',
-        destination: '/docs/guides/adding-authentication-with-auth0',
-        permanent: true,
-      },
-      {
-        source: '/docs/monitoring-and-tracing',
-        destination: '/docs/guides/monitoring-and-tracing',
-        permanent: true,
-      },
-      {
-        source: '/docs/using-graphql-features-from-the-future',
-        destination: '/docs/guides/using-graphql-features-from-the-future',
-        permanent: true,
-      },
-      {
-        source: '/docs/resolving-subscription-data-loader-caching-issues',
-        destination: '/docs/guides/resolving-subscription-data-loader-caching-issues',
-        permanent: true,
-      },
-      {
-        source: '/docs/adding-a-graphql-response-cache',
-        destination: '/docs/guides/adding-a-graphql-response-cache',
-        permanent: true,
-      },
-    ];
-  },
+  redirects: () => [
+    /* Guides */
+    {
+      source: '/docs/securing-your-graphql-api',
+      destination: '/docs/guides/securing-your-graphql-api',
+      permanent: true,
+    },
+    {
+      source: '/docs/adding-authentication-with-auth0',
+      destination: '/docs/guides/adding-authentication-with-auth0',
+      permanent: true,
+    },
+    {
+      source: '/docs/monitoring-and-tracing',
+      destination: '/docs/guides/monitoring-and-tracing',
+      permanent: true,
+    },
+    {
+      source: '/docs/using-graphql-features-from-the-future',
+      destination: '/docs/guides/using-graphql-features-from-the-future',
+      permanent: true,
+    },
+    {
+      source: '/docs/resolving-subscription-data-loader-caching-issues',
+      destination: '/docs/guides/resolving-subscription-data-loader-caching-issues',
+      permanent: true,
+    },
+    {
+      source: '/docs/adding-a-graphql-response-cache',
+      destination: '/docs/guides/adding-a-graphql-response-cache',
+      permanent: true,
+    },
+  ],
 });
