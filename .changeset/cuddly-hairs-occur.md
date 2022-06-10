2135961761e13415b6edabff5d0739517e822e3f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-10 18:38:43 +0300

    Better Context Type inheritance by exporting given dataSources input in Plugin<TDataSources>

---
+++ .changeset/cuddly-hairs-occur.md
@@ -0,0 +1,5 @@
+---
+'@envelop/apollo-datasources': patch
+---
+
+Better Context Type inheritance by exporting given dataSources input in Plugin<TDataSources>
--- packages/plugins/apollo-datasources/src/index.ts
+++ packages/plugins/apollo-datasources/src/index.ts
@@ -3,34 +3,32 @@ import { InMemoryLRUCache } from 'apollo-server-caching';
 import type { KeyValueCache } from 'apollo-server-caching';
 import type { DataSource } from 'apollo-datasource';
 
-export interface ApolloDataSourcesConfig {
-  dataSources(): {
-    [name: string]: DataSource;
-  };
+export interface ApolloDataSourcesConfig<DataSources extends Record<string, DataSource>> {
+  dataSources(): DataSources;
   cache?: KeyValueCache;
 }
 
-export function useApolloDataSources(config: ApolloDataSourcesConfig): Plugin {
+export function useApolloDataSources<DataSources extends Record<string, DataSource>>(
+  config: ApolloDataSourcesConfig<DataSources>
+): Plugin<{
+  dataSources: DataSources;
+}> {
   const cache = config.cache || new InMemoryLRUCache();
 
   return {
     async onExecute({ extendContext, args }) {
       const dataSources = config.dataSources();
-      const initializers: Array<void | Promise<void>> = [];
-      for (const dataSource of Object.values(dataSources)) {
-        if (dataSource.initialize) {
-          initializers.push(
-            dataSource.initialize({
-              context: args.contextValue,
-              cache,
-            })
-          );
-        }
-      }
 
-      await Promise.all(initializers);
+      await Promise.all(
+        Object.values(dataSources).map(dataSource =>
+          dataSource.initialize?.({
+            context: args.contextValue,
+            cache,
+          })
+        )
+      );
 
-      if ('dataSources' in args.contextValue) {
+      if (args.contextValue.dataSources != null) {
         throw new Error('Please use the dataSources config option instead of putting dataSources on the context yourself.');
       }
 
