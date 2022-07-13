d7dd95fe440bce38ad60cf7210e8d369643f9c1e
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-07-12 22:05:56 -0400

    try this

--- packages/plugins/graphql-executor/src/index.ts
+++ packages/plugins/graphql-executor/src/index.ts
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck It will work fine it is just the difference in Function signatures.
 import { Plugin } from '@envelop/core';
 import { execute } from 'graphql-executor';
 
 export const useGraphQLExecutor = (): Plugin => {
   return {
     onExecute: async ({ setExecuteFn }) => {
-      // @ts-expect-error It will work fine it is just the difference in Function signatures.
       setExecuteFn(function executorExecute(args) {
-        // @ts-expect-error It will work fine it is just the difference in Function signatures.
         return execute(args);
       });
     },
