829af17157e68b738039fa5cb94acfca3cec6093
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-07-29 14:45:16 -0400

    TEMP DISABLE LIVE QUERY BUILDING

--- packages/plugins/live-query/src/index.ts
+++ packages/plugins/live-query/src/index.ts
@@ -1,33 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck MAKE ME WORK
-import { Plugin } from '@envelop/core';
-import { print } from '@graphql-tools/graphql';
-import { NoLiveMixedWithDeferStreamRule, GraphQLLiveDirective } from '@n1ru4l/graphql-live-query';
-import type { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
-import { astFromDirective } from '@graphql-tools/utils';
+// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// // @ts-nocheck MAKE ME WORK
+// import { Plugin } from '@envelop/core';
+// import { print } from '@graphql-tools/graphql';
+// import { NoLiveMixedWithDeferStreamRule, GraphQLLiveDirective } from '@n1ru4l/graphql-live-query';
+// import type { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
+// import { astFromDirective } from '@graphql-tools/utils';
 
-export type UseLiveQueryOptions = {
-  liveQueryStore: InMemoryLiveQueryStore;
-};
+// export type UseLiveQueryOptions = {
+//   liveQueryStore: InMemoryLiveQueryStore;
+// };
 
-export { GraphQLLiveDirective };
-export const GraphQLLiveDirectiveAST = astFromDirective(GraphQLLiveDirective);
-export const GraphQLLiveDirectiveSDL = print(GraphQLLiveDirectiveAST);
+// export { GraphQLLiveDirective };
+// export const GraphQLLiveDirectiveAST = astFromDirective(GraphQLLiveDirective);
+// export const GraphQLLiveDirectiveSDL = print(GraphQLLiveDirectiveAST);
 
-export const useLiveQuery = (opts: UseLiveQueryOptions): Plugin => {
-  return {
-    onExecute: ({ executeFn, setExecuteFn }) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore  execute typings do not include AsyncIterable return right now
-      setExecuteFn(opts.liveQueryStore.makeExecute(executeFn));
-    },
-    onValidate: ({ addValidationRule }) => {
-      addValidationRule(NoLiveMixedWithDeferStreamRule);
-    },
-    onContextBuilding: ({ extendContext }) => {
-      extendContext({
-        liveQueryStore: opts.liveQueryStore,
-      });
-    },
-  };
-};
+// export const useLiveQuery = (opts: UseLiveQueryOptions): Plugin => {
+//   return {
+//     onExecute: ({ executeFn, setExecuteFn }) => {
+//       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//       // @ts-ignore  execute typings do not include AsyncIterable return right now
+//       setExecuteFn(opts.liveQueryStore.makeExecute(executeFn));
+//     },
+//     onValidate: ({ addValidationRule }) => {
+//       addValidationRule(NoLiveMixedWithDeferStreamRule);
+//     },
+//     onContextBuilding: ({ extendContext }) => {
+//       extendContext({
+//         liveQueryStore: opts.liveQueryStore,
+//       });
+//     },
+//   };
+// };
+
+export const foo = () => {};
