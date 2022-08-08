8022dad58219660ade668faebe3e25f7d5e1940b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-08-08 19:08:57 -0400

    make live query work

--- packages/plugins/live-query/src/index.ts
+++ packages/plugins/live-query/src/index.ts
@@ -1,35 +1,61 @@
-// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// // @ts-nocheck MAKE ME WORK
-// import { Plugin } from '@envelop/core';
-// import { print } from '@graphql-tools/graphql';
-// import { NoLiveMixedWithDeferStreamRule, GraphQLLiveDirective } from '@n1ru4l/graphql-live-query';
-// import type { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
-// import { astFromDirective } from '@graphql-tools/utils';
+import { Plugin } from '@envelop/core';
+import { print, GraphQLError, DefinitionNode, DirectiveNode } from '@graphql-tools/graphql';
+import { GraphQLLiveDirective } from '@n1ru4l/graphql-live-query';
+import type { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
+import { astFromDirective } from '@graphql-tools/utils';
 
-// export type UseLiveQueryOptions = {
-//   liveQueryStore: InMemoryLiveQueryStore;
-// };
+type None = null | undefined;
+const isNone = (input: unknown): input is None => input == null;
+type Maybe<T> = T | None;
 
-// export { GraphQLLiveDirective };
-// export const GraphQLLiveDirectiveAST = astFromDirective(GraphQLLiveDirective);
-// export const GraphQLLiveDirectiveSDL = print(GraphQLLiveDirectiveAST);
+export type UseLiveQueryOptions = {
+  liveQueryStore: InMemoryLiveQueryStore;
+};
 
-// export const useLiveQuery = (opts: UseLiveQueryOptions): Plugin => {
-//   return {
-//     onExecute: ({ executeFn, setExecuteFn }) => {
-//       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//       // @ts-ignore  execute typings do not include AsyncIterable return right now
-//       setExecuteFn(opts.liveQueryStore.makeExecute(executeFn));
-//     },
-//     onValidate: ({ addValidationRule }) => {
-//       addValidationRule(NoLiveMixedWithDeferStreamRule);
-//     },
-//     onContextBuilding: ({ extendContext }) => {
-//       extendContext({
-//         liveQueryStore: opts.liveQueryStore,
-//       });
-//     },
-//   };
-// };
+export { GraphQLLiveDirective };
+export const GraphQLLiveDirectiveAST = astFromDirective(GraphQLLiveDirective);
+export const GraphQLLiveDirectiveSDL = print(GraphQLLiveDirectiveAST);
 
-export const foo = () => {};
+export const getLiveDirectiveNode = (input: DefinitionNode): Maybe<DirectiveNode> => {
+  if (input.kind !== 'OperationDefinition' || input.operation !== 'query') {
+    return null;
+  }
+  const liveDirective = input.directives?.find(d => d.name.value === 'live');
+  if (isNone(liveDirective)) {
+    return null;
+  }
+
+  return liveDirective;
+};
+
+export const useLiveQuery = (opts: UseLiveQueryOptions): Plugin => {
+  return {
+    onExecute: ({ executeFn, setExecuteFn }) => {
+      setExecuteFn(opts.liveQueryStore.makeExecute(executeFn));
+    },
+    onValidate: ({ addValidationRule }) => {
+      addValidationRule(context => {
+        return {
+          OperationDefinition(operationDefinitionNode) {
+            if (isNone(getLiveDirectiveNode(operationDefinitionNode))) {
+              return false;
+            }
+            return undefined;
+          },
+          Directive(directiveNode) {
+            if (directiveNode.name.value === 'defer' || directiveNode.name.value === 'stream') {
+              context.reportError(
+                new GraphQLError(`Cannot mix "@${directiveNode.name.value}" with "@live".`, directiveNode.name)
+              );
+            }
+          },
+        };
+      });
+    },
+    onContextBuilding: ({ extendContext }) => {
+      extendContext({
+        liveQueryStore: opts.liveQueryStore,
+      });
+    },
+  };
+};
--- packages/plugins/live-query/test/use-live-query.spec.ts
+++ packages/plugins/live-query/test/use-live-query.spec.ts
@@ -1,71 +1,69 @@
-// import { useLiveQuery, GraphQLLiveDirectiveSDL } from '../src/index.js';
-// import { makeExecutableSchema } from '@graphql-tools/schema';
-// import { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
-// import { createTestkit, assertStreamExecutionValue } from '@envelop/testing';
-// import { parse } from '@graphql-tools/graphql';
+import { useLiveQuery, GraphQLLiveDirectiveSDL } from '../src/index.js';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { InMemoryLiveQueryStore } from '@n1ru4l/in-memory-live-query-store';
+import { createTestkit, assertStreamExecutionValue } from '@envelop/testing';
 
-// const schema = makeExecutableSchema({
-//   typeDefs: [
-//     /* GraphQL */ `
-//       type Query {
-//         greetings: [String!]
-//       }
-//     `,
-//     GraphQLLiveDirectiveSDL,
-//   ],
-//   resolvers: {
-//     Query: {
-//       greetings: (_: unknown, __: unknown, context) => context.greetings,
-//     },
-//   },
-// });
+const schema = makeExecutableSchema({
+  typeDefs: [
+    /* GraphQL */ `
+      type Query {
+        greetings: [String!]
+      }
+    `,
+    GraphQLLiveDirectiveSDL,
+  ],
+  resolvers: {
+    Query: {
+      greetings: (_: unknown, __: unknown, context) => context.greetings,
+    },
+  },
+});
 
-// // TODO: MAKE ME WORK
-// describe.skip('useLiveQuery', () => {
-//   it('works with simple schema', async () => {
-//     const liveQueryStore = new InMemoryLiveQueryStore();
-//     const testKit = createTestkit([useLiveQuery({ liveQueryStore })], schema);
-//     const contextValue = {
-//       greetings: ['Hi', 'Sup', 'Ola'],
-//     };
-//     const result = await testKit.execute(
-//       /* GraphQL */ `
-//         query @live {
-//           greetings
-//         }
-//       `,
-//       undefined,
-//       contextValue
-//     );
-//     assertStreamExecutionValue(result);
-//     let current = await result.next();
-//     expect(current.value).toMatchInlineSnapshot(`
-// Object {
-//   "data": Object {
-//     "greetings": Array [
-//       "Hi",
-//       "Sup",
-//       "Ola",
-//     ],
-//   },
-//   "isLive": true,
-// }
-// `);
-//     contextValue.greetings.reverse();
-//     liveQueryStore.invalidate('Query.greetings');
-//     current = await result.next();
-//     expect(current.value).toMatchInlineSnapshot(`
-// Object {
-//   "data": Object {
-//     "greetings": Array [
-//       "Ola",
-//       "Sup",
-//       "Hi",
-//     ],
-//   },
-//   "isLive": true,
-// }
-// `);
-//     result.return?.();
-//   });
-// });
+describe('useLiveQuery', () => {
+  it('works with simple schema', async () => {
+    const liveQueryStore = new InMemoryLiveQueryStore();
+    const testKit = createTestkit([useLiveQuery({ liveQueryStore })], schema);
+    const contextValue = {
+      greetings: ['Hi', 'Sup', 'Ola'],
+    };
+    const result = await testKit.execute(
+      /* GraphQL */ `
+        query @live {
+          greetings
+        }
+      `,
+      undefined,
+      contextValue
+    );
+    assertStreamExecutionValue(result);
+    let current = await result.next();
+    expect(current.value).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "greetings": Array [
+      "Hi",
+      "Sup",
+      "Ola",
+    ],
+  },
+  "isLive": true,
+}
+`);
+    contextValue.greetings.reverse();
+    liveQueryStore.invalidate('Query.greetings');
+    current = await result.next();
+    expect(current.value).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "greetings": Array [
+      "Ola",
+      "Sup",
+      "Hi",
+    ],
+  },
+  "isLive": true,
+}
+`);
+    result.return?.();
+  });
+});
