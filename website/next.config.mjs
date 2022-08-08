1200c38f2ccb2a985df37d2ae16ab0c4d7c03629
TuvalSimha <tuval.simha@gmail.com>
2022-08-08 12:22:11 +0300

    bootstrap
    getting-started
    
    plugins
    
    guides
    
    plugins
    
    plugins
    
    cleanup, update to latest, fixes
    
    main components page works now
    
    wip plugin page
    
    fixes
    
    fixes
    
    all fixes
    
    prettify
    
    fixes
    
    remove unused
    
    added website pipeline

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,55 +1,27 @@
-import { createRequire } from 'node:module';
-import { withGuildDocs } from '@guild-docs/server';
-import { register } from 'esbuild-register/dist/node.js';
-import { i18n } from './next-i18next.config.js';
-
-const require = createRequire(import.meta.url);
-register({ extensions: ['.ts', '.tsx'] });
-
-const { getRoutes } = require('./routes.ts');
+import { withGuildDocs } from '@theguild/components/next.config';
 
 export default withGuildDocs({
-  i18n,
-  getRoutes,
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    unoptimized: true,
   },
-  redirects: () => [
-    /* Guides */
-    {
-      source: '/docs/securing-your-graphql-api',
-      destination: '/docs/guides/securing-your-graphql-api',
+  experimental: {
+    urlImports: ['https://the-guild.dev/static/shared-logos/products/hive.svg'],
+  },
+  redirects: () =>
+    Object.entries({
+      '/docs/securing-your-graphql-api': '/docs/guides/securing-your-graphql-api',
+      '/docs/adding-authentication-with-auth0': '/docs/guides/adding-authentication-with-auth0',
+      '/docs/monitoring-and-tracing': '/docs/guides/monitoring-and-tracing',
+      '/docs/using-graphql-features-from-the-future': '/docs/guides/using-graphql-features-from-the-future',
+      '/docs/resolving-subscription-data-loader-caching-issues':
+        '/docs/guides/resolving-subscription-data-loader-caching-issues',
+      '/docs/adding-a-graphql-response-cache': '/docs/guides/adding-a-graphql-response-cache',
+      '/plugins/use-depth-limit': '/plugins/graphql-armor-max-depth',
+      '/docs/introduction': '/docs',
+      '/docs/plugins/introduction': '/docs/plugins',
+    }).map(([from, to]) => ({
+      source: from,
+      destination: to,
       permanent: true,
-    },
-    {
-      source: '/docs/adding-authentication-with-auth0',
-      destination: '/docs/guides/adding-authentication-with-auth0',
-      permanent: true,
-    },
-    {
-      source: '/docs/monitoring-and-tracing',
-      destination: '/docs/guides/monitoring-and-tracing',
-      permanent: true,
-    },
-    {
-      source: '/docs/using-graphql-features-from-the-future',
-      destination: '/docs/guides/using-graphql-features-from-the-future',
-      permanent: true,
-    },
-    {
-      source: '/docs/resolving-subscription-data-loader-caching-issues',
-      destination: '/docs/guides/resolving-subscription-data-loader-caching-issues',
-      permanent: true,
-    },
-    {
-      source: '/docs/adding-a-graphql-response-cache',
-      destination: '/docs/guides/adding-a-graphql-response-cache',
-      permanent: true,
-    },
-    {
-      source: '/plugins/use-depth-limit',
-      destination: '/plugins/graphql-armor-max-depth',
-      permanent: true,
-    },
-  ],
+    })),
 });
---
+++ website/src/lib/plugin-get-static-props.ts
@@ -0,0 +1,55 @@
+import { compileMdx } from 'nextra/compile';
+import { fetchPackageInfo } from '@theguild/components';
+import { pluginsArr } from './plugins';
+import { format } from 'date-fns';
+import { GetStaticProps, GetStaticPaths } from 'next';
+
+export const getStaticPaths: GetStaticPaths = () => ({
+  fallback: 'blocking',
+  paths: pluginsArr.map(({ identifier }) => ({
+    params: { name: identifier },
+  })),
+});
+
+export const getStaticProps: GetStaticProps = async ctx => {
+  const pluginPath = ctx.params?.name;
+  const plugin = pluginsArr.find(v => v.identifier === pluginPath);
+
+  if (!plugin) {
+    throw new Error(`Unknown "${pluginPath}" plugin identifier`);
+  }
+  const { npmPackage, githubReadme, title } = plugin;
+  const { readme, updatedAt } = await fetchPackageInfo(npmPackage, githubReadme);
+
+  const mdx = await compileMdx(
+    `
+# \`${title}\`
+
+|Package name|Weekly Downloads|Version|License|Updated|
+|-|-|-|-|-|
+|[\`${npmPackage}\`](https://npmjs.com/package/${npmPackage})|![Downloads](https://badgen.net/npm/dw/${npmPackage} "Downloads")|![Version](https://badgen.net/npm/v/${npmPackage} "Version")|![License](https://badgen.net/npm/license/${npmPackage} "License")|${format(
+      new Date(updatedAt),
+      'MMM do, yyyy'
+    )}|
+
+## Installation
+
+<PackageCmd packages={["${npmPackage}"]} />
+
+${readme}`,
+    {
+      mdxOptions: {
+        outputFormat: 'function-body',
+        jsx: false,
+      },
+    }
+  );
+
+  return {
+    props: {
+      ssg: mdx.result,
+    },
+    // The page will be considered as stale and regenerated every 24 hours.
+    revalidate: 60 * 60 * 24,
+  };
+};
