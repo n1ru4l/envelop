3ccdd020eb1a4f9be85244e041d4670bf72efe5b
enisdenjo <badurinadenis@gmail.com>
2022-09-14 16:54:50 +0200

    perform takes apollo-server request

--- examples/apollo-server/index.ts
+++ examples/apollo-server/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { ApolloServer } from 'apollo-server';
-import { envelop, useSchema, useTiming } from '@envelop/core';
+import { envelop, isAsyncIterable, useSchema, useTiming } from '@envelop/core';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
@@ -25,12 +25,11 @@ const server = new ApolloServer({
   schema,
   executor: async requestContext => {
     const { perform } = getEnveloped({ req: requestContext.request.http });
-
-    return perform({
-      query: requestContext.document,
-      variables: requestContext.request.variables,
-      operationName: requestContext.operationName,
-    });
+    const result = await perform(requestContext.request);
+    if (isAsyncIterable(result)) {
+      throw new Error('Unsupported streaming result');
+    }
+    return result;
   },
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground({ endpoint: '/graphql' })],
 });
