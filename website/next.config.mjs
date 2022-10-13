1c10621cb27695ad0f2d1f4d975c9a6dcb582209
Dotan Simha <dotansimha@gmail.com>
2022-10-13 20:48:14 +0900

    Upgrade website setup to latest

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,9 +1,6 @@
 import { withGuildDocs } from '@theguild/components/next.config';
 
 export default withGuildDocs({
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     urlImports: ['https://the-guild.dev/static/shared-logos/products/hive.svg'],
   },
