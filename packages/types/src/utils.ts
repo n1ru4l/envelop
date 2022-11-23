b8ca1f1a9409c8434684eca518bd845664aa15e4
enisdenjo <badurinadenis@gmail.com>
2022-11-23 18:33:55 +0100

    remove id util

--- packages/types/src/utils.ts
+++ packages/types/src/utils.ts
@@ -2,14 +2,10 @@ export type OptionalPropertyNames<T> = { [K in keyof T]-?: {} extends { [P in K]
 
 export type SpreadProperties<L, R, K extends keyof L & keyof R> = { [P in K]: L[P] | Exclude<R[P], undefined> };
 
-export type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
-
-export type SpreadTwo<L, R> = Id<
-  Pick<L, Exclude<keyof L, keyof R>> &
-    Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>> &
-    Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>> &
-    SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
->;
+export type SpreadTwo<L, R> = Pick<L, Exclude<keyof L, keyof R>> &
+  Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>> &
+  Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>> &
+  SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>;
 
 export type Spread<A extends readonly [...any]> = A extends [infer L, ...infer R] ? SpreadTwo<L, Spread<R>> : {};
 
