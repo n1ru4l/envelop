db20864aac3fcede3e265ae63b2e8cb4664ba23a
Valentin Cocaud <v.cocaud@gmail.com>
2023-09-27 23:15:49 +0200

    fix multiple nested envelop extension duplicating plugins (#1944)
    * fix multiple nested envelop extension duplicating plugins
    
    * changeset
    
    * chore(dependencies): updated changesets for modified dependencies
    
    * Use a flag instead of modifying the plugin
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- packages/core/src/plugins/use-envelop.ts
+++ packages/core/src/plugins/use-envelop.ts
@@ -1,11 +1,17 @@
 import { GetEnvelopedFn, Plugin } from '@envelop/types';
 
 export const useEnvelop = (envelop: GetEnvelopedFn<any>): Plugin<any> => {
+  let initialized = false;
   return {
     onPluginInit({ addPlugin }) {
+      if (initialized) {
+        return;
+      }
       for (const plugin of envelop._plugins) {
         addPlugin(plugin);
       }
+      // Avoid double execution if envelop is extended multiple times
+      initialized = true;
     },
   };
 };
