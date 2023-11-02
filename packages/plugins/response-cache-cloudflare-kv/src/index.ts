94c792438cf01b0aee82bd3de89f569a59409c5e
Adishwar Rishi <adiswa123@gmail.com>
2023-11-02 22:16:58 +1100

    Add @envelop/response-cache-cloudflare-kv

---
+++ packages/plugins/response-cache-cloudflare-kv/src/index.ts
@@ -0,0 +1,72 @@
+import type { ExecutionResult } from 'graphql';
+import type { ExecutionContext, KVNamespace } from '@cloudflare/workers-types';
+import type { Cache, CacheEntityRecord } from '@envelop/response-cache';
+import { buildOperationKey } from './cache-key';
+import { invalidate } from './invalidate';
+import { set } from './set';
+
+export type KvCacheConfig = {
+  /**
+   * The Cloudflare KV namespace that should be used to store the cache
+   */
+  KV: KVNamespace;
+  /**
+   * The Cloudflare worker execution context. Used to perform non-blocking actions like cache storage and invalidation.
+   */
+  ctx: ExecutionContext;
+  /**
+   *  Defines the length of time in milliseconds that a KV result is cached in the global network location it is accessed from.
+   *
+   * The cacheTTL parameter must be an integer greater than or equal to 60000 (60 seconds), which is the default.
+   */
+  cacheReadTTL?: number;
+  /**
+   * A prefix that should be added to all cache keys
+   */
+  keyPrefix?: string;
+};
+
+/**
+ * Creates a cache object that uses Cloudflare KV to store GraphQL responses.
+ * This cache is optimized for Cloudflare workers and uses the `ctx.waitUntil` method to perform non-blocking actions where possible
+ *
+ * To find out more about how this cache is implemented see https://the-guild.dev/blog/graphql-response-caching-with-envelop
+ *
+ * @param config Modify the behavior of the cache as it pertains to Cloudflare KV
+ * @returns A cache object that can be passed to envelop's `useResponseCache` plugin
+ */
+export function createKvCache(config: KvCacheConfig): Cache {
+  const cache: Cache = {
+    async get(id: string) {
+      const ttlInSeconds = Math.max(Math.floor((config.cacheReadTTL ?? 60000) / 1000), 60); // KV TTL must be at least 60 seconds
+      const kvResponse = await config.KV.get(buildOperationKey(id, config.keyPrefix), {
+        type: 'text',
+        cacheTtl: ttlInSeconds,
+      });
+      if (kvResponse) {
+        return JSON.parse(kvResponse) as ExecutionResult;
+      }
+      return undefined;
+    },
+
+    set(
+      /** id/hash of the operation */
+      id: string,
+      /** the result that should be cached */
+      data: ExecutionResult,
+      /** array of entity records that were collected during execution */
+      entities: Iterable<CacheEntityRecord>,
+      /** how long the operation should be cached (in milliseconds) */
+      ttl: number,
+    ) {
+      // Do not block execution of the worker while caching the result
+      config.ctx.waitUntil(set(id, data, entities, ttl, config));
+    },
+
+    invalidate(entities: Iterable<CacheEntityRecord>) {
+      // Do not block execution of the worker while invalidating the cache
+      config.ctx.waitUntil(invalidate(entities, config));
+    },
+  };
+  return cache;
+}
---
+++ packages/plugins/response-cache-cloudflare-kv/src/invalidate.ts
@@ -0,0 +1,44 @@
+import type { CacheEntityRecord } from '@envelop/response-cache';
+import type { KvCacheConfig } from '.';
+import { buildEntityKey } from './cache-key';
+
+export async function invalidate(
+  entities: Iterable<CacheEntityRecord>,
+  config: KvCacheConfig,
+): Promise<void> {
+  const kvPromises: Promise<unknown>[] = []; // Collecting all the KV operations so we can await them all at once
+
+  for (const entity of entities) {
+    const entityKey = buildEntityKey(entity.typename, entity.id, config.keyPrefix);
+
+    for await (const kvKey of _getAllKvKeysForPrefix(entityKey, config)) {
+      if (kvKey.metadata?.operationKey) {
+        kvPromises.push(config.KV.delete(kvKey.metadata?.operationKey));
+        kvPromises.push(config.KV.delete(kvKey.name));
+      }
+    }
+  }
+
+  await Promise.allSettled(kvPromises);
+}
+
+export async function* _getAllKvKeysForPrefix(prefix: string, config: KvCacheConfig) {
+  let keyListComplete = false;
+  let cursor: string | undefined;
+
+  do {
+    const kvListResponse = await config.KV.list<{ operationKey: string }>({
+      prefix,
+      cursor,
+    });
+    keyListComplete = kvListResponse.list_complete;
+
+    if (!kvListResponse.list_complete) {
+      cursor = kvListResponse.cursor;
+    }
+
+    for (const keyResult of kvListResponse.keys) {
+      yield keyResult;
+    }
+  } while (!keyListComplete);
+}
