80aa9d70fd2d7cca12ca0ffe612084d0dcc54e3f
Adishwar Rishi <adiswa123@gmail.com>
2023-11-06 13:53:19 +1100

    Add unit tests and re-configure the jest setup to handle projects of different types

--- jest.config.js
+++ jest.config.js
@@ -6,17 +6,33 @@ const ROOT_DIR = __dirname;
 const TSCONFIG = resolve(ROOT_DIR, 'tsconfig.json');
 const tsconfig = require(TSCONFIG);
 
+/** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
   rootDir: ROOT_DIR,
-  restoreMocks: true,
+  projects: [
+    {
+      displayName: 'Standard Tests',
+      testMatch: [
+        '<rootDir>/**/*.spec.ts',
+        '<rootDir>/**/*.spec.js',
+        '<rootDir>/**/*.test.ts',
+        '<rootDir>/**/*.test.js',
+      ],
+      testPathIgnorePatterns: ['<rootDir>/packages/plugins/response-cache-cloudflare-kv'],
+      restoreMocks: true,
+      modulePathIgnorePatterns: ['dist', 'test-assets', 'test-files', 'fixtures', '.bob'],
+      moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+        prefix: `${ROOT_DIR}/`,
+      }),
+      cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
+      resolver: 'bob-the-bundler/jest-resolver',
+      setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+    },
+    // Cloudflare plugin tests need very different build settings
+    // giving Jest a string as project name will make it rely on jest.config files in the package subfolder
+    '<rootDir>/packages/plugins/response-cache-cloudflare-kv',
+  ],
   reporters: ['default'],
-  modulePathIgnorePatterns: ['dist', 'test-assets', 'test-files', 'fixtures', '.bob'],
-  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
-    prefix: `${ROOT_DIR}/`,
-  }),
   collectCoverage: false,
-  cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
-  resolver: 'bob-the-bundler/jest-resolver',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
 };
