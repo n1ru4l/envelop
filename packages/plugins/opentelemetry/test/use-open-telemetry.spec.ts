43051522725bdc6da15fe995cdcb731a015777b6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-01-18 14:20:52 +0300

    feat(otel): subscriptions support & traceId in the extensions

--- packages/plugins/opentelemetry/test/use-open-telemetry.spec.ts
+++ packages/plugins/opentelemetry/test/use-open-telemetry.spec.ts
@@ -1,11 +1,16 @@
-import { buildSchema } from 'graphql';
+import { buildSchema, GraphQLError } from 'graphql';
 import { assertSingleExecutionValue, createTestkit } from '@envelop/testing';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import * as opentelemetry from '@opentelemetry/api';
+import { SpanStatusCode } from '@opentelemetry/api';
+import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
 import {
   BasicTracerProvider,
   InMemorySpanExporter,
   SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
-import { useOpenTelemetry } from '../src/index.js';
+import { Repeater } from '@repeaterjs/repeater';
+import { AttributeName, TracingOptions, useOpenTelemetry } from '../src/index.js';
 
 function createTraceProvider(exporter: InMemorySpanExporter) {
   const provider = new BasicTracerProvider();
@@ -15,19 +20,51 @@ function createTraceProvider(exporter: InMemorySpanExporter) {
   return provider;
 }
 
-describe('useOpenTelemetry', () => {
-  const schema = buildSchema(/* GraphQL */ `
-    type Query {
-      ping: String
-    }
-  `);
-  const query = /* GraphQL */ `
-    query {
-      ping
-    }
-  `;
+const contextManager = new AsyncLocalStorageContextManager().enable();
+opentelemetry.context.setGlobalContextManager(contextManager);
 
-  const useTestOpenTelemetry = (exporter?: InMemorySpanExporter, options?: any) =>
+describe('useOpenTelemetry', () => {
+  const schema = makeExecutableSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        ping: String
+        echo(message: String): String
+        error: String
+        context: String
+      }
+
+      type Subscription {
+        counter(count: Int!): Int!
+      }
+    `,
+    resolvers: {
+      Query: {
+        ping: () => {
+          return 'pong';
+        },
+        echo: (_, { message }) => {
+          return `echo: ${message}`;
+        },
+        error: () => {
+          throw new GraphQLError('boom');
+        },
+      },
+      Subscription: {
+        counter: {
+          subscribe: (_, args) => {
+            return new Repeater((push, end) => {
+              for (let i = args.count; i >= 0; i--) {
+                push({ counter: i });
+              }
+              end();
+            });
+          },
+        },
+      },
+    },
+  });
+
+  const useTestOpenTelemetry = (exporter?: InMemorySpanExporter, options?: TracingOptions) =>
     useOpenTelemetry(
       {
         resolvers: false,
@@ -37,6 +74,12 @@ describe('useOpenTelemetry', () => {
       },
       exporter ? createTraceProvider(exporter) : undefined,
     );
+
+  const pingQuery = /* GraphQL */ `
+    query {
+      ping
+    }
+  `;
 
   it('Should override execute function', async () => {
     const onExecuteSpy = jest.fn();
@@ -50,7 +93,7 @@ describe('useOpenTelemetry', () => {
       schema,
     );
 
-    const result = await testInstance.execute(query);
+    const result = await testInstance.execute(pingQuery);
     assertSingleExecutionValue(result);
     expect(onExecuteSpy).toHaveBeenCalledTimes(1);
   });
@@ -59,10 +102,10 @@ describe('useOpenTelemetry', () => {
     const exporter = new InMemorySpanExporter();
     const testInstance = createTestkit([useTestOpenTelemetry(exporter)], schema);
 
-    await testInstance.execute(query);
+    await testInstance.execute(pingQuery);
     const actual = exporter.getFinishedSpans();
     expect(actual.length).toBe(1);
-    expect(actual[0].name).toBe('Anonymous Operation');
+    expect(actual[0].name).toBe('query.anonymous');
   });
 
   it('Should add resolver span if requested', async () => {
@@ -72,10 +115,262 @@ describe('useOpenTelemetry', () => {
       schema,
     );
 
-    await testInstance.execute(query);
+    await testInstance.execute(pingQuery);
     const actual = exporter.getFinishedSpans();
     expect(actual.length).toBe(2);
     expect(actual[0].name).toBe('Query.ping');
-    expect(actual[1].name).toBe('Anonymous Operation');
+    expect(actual[1].name).toBe('query.anonymous');
+  });
+
+  it('query should add trace_id to extensions', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit(
+      [useTestOpenTelemetry(exporter, { traceIdInResult: 'trace_id' })],
+      schema,
+    );
+
+    const result = await testInstance.execute(pingQuery);
+    assertSingleValue(result);
+    expect(result.extensions.trace_id).toBeTruthy();
+  });
+
+  it('execute span should use the operation name', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit([useTestOpenTelemetry(exporter)], schema);
+
+    await testInstance.execute(/* GraphQL */ `
+      query ping {
+        ping
+      }
+    `);
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].name).toBe('query.ping');
+  });
+
+  it('execute span should add attributes', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit([useTestOpenTelemetry(exporter)], schema);
+
+    const queryStr = /* GraphQL */ `
+      query echo($message: String!) {
+        echo(message: $message)
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr, {
+      message: 'hello',
+    });
+
+    assertSingleValue(resp);
+    expect(resp.data).toEqual({ echo: 'echo: hello' });
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_DOCUMENT]: queryStr,
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'echo',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'query',
+    });
+  });
+
+  it('execute span should not add attribute if has inline variable', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit(
+      [useTestOpenTelemetry(exporter, { variables: false })],
+      schema,
+    );
+
+    const queryStr = /* GraphQL */ `
+      query echo {
+        echo(message: "hello")
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr);
+
+    assertSingleValue(resp);
+    expect(resp.data).toEqual({ echo: 'echo: hello' });
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'echo',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'query',
+    });
+  });
+
+  it('span should not add document attribute if options false', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit(
+      [useTestOpenTelemetry(exporter, { document: false })],
+      schema,
+    );
+
+    const queryStr = /* GraphQL */ `
+      query echo($message: String!) {
+        echo(message: $message)
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr, {
+      message: 'hello',
+    });
+
+    assertSingleValue(resp);
+    expect(resp.data).toEqual({ echo: 'echo: hello' });
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'echo',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'query',
+    });
+  });
+
+  it('span should include error attribute', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit([useTestOpenTelemetry(exporter)], schema);
+
+    const queryStr = /* GraphQL */ `
+      query error {
+        error
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr);
+
+    assertSingleValue(resp);
+    expect(resp.errors).toBeTruthy();
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_DOCUMENT]: queryStr,
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'error',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'query',
+    });
+    expect(actual[0].status.code).toEqual(SpanStatusCode.ERROR);
+  });
+
+  it('should add subscription span', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit([useTestOpenTelemetry(exporter)], schema);
+
+    const queryStr = /* GraphQL */ `
+      subscription counter($count: Int!) {
+        counter(count: $count)
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr, {
+      count: 5,
+    });
+
+    assertAsyncIterable(resp);
+
+    let expected = 5;
+    for await (const value of resp) {
+      expect(value.data?.counter).toBe(expected); // ensure subscription works
+      expected--;
+    }
+
+    await setTimeout$(100); // allow the server some grace to close the graphql.request span after the response finishes
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].name).toBe('subscription.counter');
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_DOCUMENT]: queryStr,
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'counter',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'subscription',
+    });
+  });
+
+  it('should not add subscription span if inline', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit([useTestOpenTelemetry(exporter)], schema);
+
+    const queryStr = /* GraphQL */ `
+      subscription {
+        counter(count: 5)
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr);
+
+    assertAsyncIterable(resp);
+
+    let expected = 5;
+    for await (const value of resp) {
+      expect(value.data?.counter).toBe(expected); // ensure subscription works
+      expected--;
+    }
+
+    await setTimeout$(100); // allow the server some grace to close the graphql.request span after the response finishes
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].name).toBe('subscription.anonymous');
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'anonymous',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'subscription',
+    });
+  });
+
+  it('subscription should add trace_id to extensions', async () => {
+    const exporter = new InMemorySpanExporter();
+    const testInstance = createTestkit(
+      [useTestOpenTelemetry(exporter, { traceIdInResult: 'trace_id' })],
+      schema,
+    );
+
+    const queryStr = /* GraphQL */ `
+      subscription counter($count: Int!) {
+        counter(count: $count)
+      }
+    `;
+
+    const resp = await testInstance.execute(queryStr, {
+      count: 5,
+    });
+
+    assertAsyncIterable(resp);
+
+    let expected = 5;
+    for await (const value of resp) {
+      expect(value.data?.counter).toBe(expected); // ensure subscription works
+      expect(value.extensions).toHaveProperty('trace_id');
+      expected--;
+    }
+
+    await setTimeout$(100); // allow the server some grace to close the graphql.request span after the response finishes
+
+    const actual = exporter.getFinishedSpans();
+    expect(actual.length).toBe(1);
+    expect(actual[0].name).toBe('subscription.counter');
+    expect(actual[0].attributes).toEqual({
+      [AttributeName.EXECUTION_OPERATION_DOCUMENT]: queryStr,
+      [AttributeName.EXECUTION_OPERATION_NAME]: 'counter',
+      [AttributeName.EXECUTION_OPERATION_TYPE]: 'subscription',
+    });
   });
 });
+
+function assertSingleValue(
+  result: any,
+): asserts result is { data: any; extensions: any; errors: any[] } {
+  expect(result).toHaveProperty('data');
+  expect(result.data).not.toBeNull();
+  expect(result.data).not.toBeUndefined();
+}
+
+function assertAsyncIterable<T>(value: any): asserts value is AsyncIterable<T> {
+  expect(value[Symbol.asyncIterator]).toBeInstanceOf(Function);
+}
+
+function setTimeout$(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
