d2f77e04c7c6bb44f04ea4e3b9a7f46b6c6910a1
Kurt Martinsen <kurt.martinsen@jamadvisors.fi>
2024-05-20 23:05:30 +0300

    chore(auth0): change audience signature to VerifyOptions['audience']

--- packages/plugins/auth0/src/index.ts
+++ packages/plugins/auth0/src/index.ts
@@ -9,7 +9,7 @@ const { decode, verify } = jwtPkg;
 
 export type Auth0PluginOptions = {
   domain: string;
-  audience: string | RegExp | Array<string | RegExp> | undefined;
+  audience: VerifyOptions['audience'];
 
   preventUnauthenticatedAccess?: boolean;
   onError?: (error: Error) => void;
