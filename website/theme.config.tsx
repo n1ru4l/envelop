8019eb4d2dc431a08f09f4159038fd70d7e93024
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-30 20:56:16 +0100

    Hide redundant logo for now

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -32,7 +32,7 @@ export default defineConfig({
   },
   websiteName: 'Envelop',
   description: 'The GraphQL plugin system',
-  logo: PRODUCTS.ENVELOP.logo({ className: 'w-8' }),
+  logo: null,
   editLink: {
     component({ children, className, filePath }) {
       const router = useRouter();
