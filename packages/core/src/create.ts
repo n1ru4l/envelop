823650d00fffc50791ddf976dfd481825b27082f
Valentin Cocaud <v.cocaud@gmail.com>
2025-02-15 14:44:01 +0100

    feat(core) New tracer API proposal

--- packages/core/src/create.ts
+++ packages/core/src/create.ts
@@ -1,5 +1,6 @@
 import { ArbitraryObject, ComposeContext, GetEnvelopedFn, Optional, Plugin } from '@envelop/types';
 import { createEnvelopOrchestrator, EnvelopOrchestrator } from './orchestrator.js';
+import { getTraced, getTracer } from './tracer.js';
 
 type ExcludeFalsy<TArray extends any[]> = Exclude<TArray[0], null | undefined | false>[];
 
@@ -12,6 +13,7 @@ export function envelop<PluginsType extends Optional<Plugin<any>>[]>(options: {
   enableInternalTracing?: boolean;
 }): GetEnvelopedFn<ComposeContext<ExcludeFalsy<PluginsType>>> {
   const plugins = options.plugins.filter(notEmpty);
+  const tracer = getTracer<ComposeContext<ExcludeFalsy<PluginsType>>>(plugins);
   const orchestrator = createEnvelopOrchestrator<ComposeContext<ExcludeFalsy<PluginsType>>>({
     plugins,
   });
@@ -19,18 +21,23 @@ export function envelop<PluginsType extends Optional<Plugin<any>>[]>(options: {
   const getEnveloped = <TInitialContext extends ArbitraryObject>(
     initialContext: TInitialContext = {} as TInitialContext,
   ) => {
+    const traced = getTraced(initialContext);
     const typedOrchestrator = orchestrator as EnvelopOrchestrator<
       TInitialContext,
       ComposeContext<ExcludeFalsy<PluginsType>>
     >;
-    typedOrchestrator.init(initialContext);
+
+    traced.sync(tracer?.init, orchestrator.init)(initialContext);
 
     return {
-      parse: typedOrchestrator.parse(initialContext),
-      validate: typedOrchestrator.validate(initialContext),
-      contextFactory: typedOrchestrator.contextFactory(initialContext as any),
-      execute: typedOrchestrator.execute,
-      subscribe: typedOrchestrator.subscribe,
+      parse: traced.sync(tracer?.parse, typedOrchestrator.parse(initialContext)),
+      validate: traced.sync(tracer?.validate, typedOrchestrator.validate(initialContext)),
+      contextFactory: traced.sync(
+        tracer?.context,
+        typedOrchestrator.contextFactory(initialContext as any),
+      ),
+      execute: traced.maybeAsync(tracer?.execute, typedOrchestrator.execute),
+      subscribe: traced.maybeAsync(tracer?.subscribe, typedOrchestrator.subscribe),
       schema: typedOrchestrator.getCurrentSchema(),
     };
   };
