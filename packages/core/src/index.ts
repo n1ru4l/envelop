7afb1ad4d04d1538afd3d9524b594b42e01a9ab7
Valentin Cocaud <v.cocaud@gmail.com>
2025-02-18 19:43:53 +0100

    export tracer utils

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,4 +11,5 @@ export * from './plugins/use-payload-formatter.js';
 export * from './plugins/use-masked-errors.js';
 export * from './plugins/use-engine.js';
 export * from './plugins/use-validation-rule.js';
+export * from './tracer.js';
 export { getDocumentString } from './document-string-map.js';
